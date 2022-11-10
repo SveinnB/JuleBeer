a475e3979351a100a218cff32e5652644deea7cc
Sveinn Bjarki Brynjarsson <Sveinn.Bjarki.Brynjarsson@origo.is>
2022-11-10 09:04:59 +0000

    fix

---
+++ Server/JuleBeer/JuleBeer/Migrations/20221110090338_skyrjarmur.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JuleBeer.Migrations
+{
+    public partial class skyrjarmur : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Beers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Skyrjarmur nr. 99");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Beers",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Skyrjarmur Skyrjarmur nr. 99");
+        }
+    }
+}
